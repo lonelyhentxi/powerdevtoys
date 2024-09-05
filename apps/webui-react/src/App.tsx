0680d008e249575fa49cd57acf34e187dd8e3824
lonelyhentxi <master@evernightfireworks.com>
2024-09-05 22:51:07 +0800

    feat: add shadcn examples to test

--- apps/webui-react/src/App.tsx
+++ apps/webui-react/src/App.tsx
@@ -1,56 +1,16 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
 import { invoke } from "@tauri-apps/api/core";
-import { Button } from '@ui/button'
-import { Input } from '@ui/input';
-import '@styles/global.css';
-import s from "./App.module.scss";
-import cx from 'clsx';
+import './global.css';
+import { FunctionsPage } from "./pages/_functions";
 
 function App() {
-  const [greetMsg, setGreetMsg] = useState("");
-  const [name, setName] = useState("");
 
-  async function greet() {
+  async function _greet() {
     // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
-    setGreetMsg(await invoke("greet", { name }));
+    await invoke("greet", { name: "powerdevtoys" })
   }
 
   return (
-    <div className="flex flex-col text-center justify-center pt-[10vh] m-0 p-16">
-      <h1 className="text-center">Welcome to Tauri!</h1>
-
-      <div className="flex flex-row items-center justify-center">
-        <a href="https://rsbuild.dev/" target="_blank">
-          <img src="/rsbuild.svg" className={cx(s.logo, s.rsbuild)} alt="Rsbuild logo" />
-        </a>
-        <a href="https://tauri.app" target="_blank">
-          <img src="/tauri.svg" className={cx(s.logo, s.tauri)} alt="Tauri logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className={cx(s.logo, s.react)} alt="React logo" />
-        </a>
-      </div>
-
-      <p>Click on the Tauri, Vite, and React logos to learn more.</p>
-
-      <form
-        className="flex flex-row items-center gap-5"
-        onSubmit={(e) => {
-          e.preventDefault();
-          greet();
-        }}
-      >
-        <Input
-          id="greet-input"
-          onChange={(e) => setName(e.currentTarget.value)}
-          placeholder="Enter a name..."
-        />
-        <Button type="submit">Greet</Button>
-      </form>
-
-      <p>{greetMsg}</p>
-    </div>
+    <FunctionsPage />
   );
 }
 
--- apps/webui-react/src/main.tsx
+++ apps/webui-react/src/main.tsx
@@ -1,8 +1,23 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import { ResizeObserver } from "@juggle/resize-observer";
 
-ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+if (!window.ResizeObserver) {
+  window.ResizeObserver = ResizeObserver;
+}
+
+const mainElementId = "root";
+const container = document.getElementById(mainElementId);
+
+
+if (!container) {
+  throw new Error(
+    `No container '${mainElementId}' found to render application`
+  );
+}
+
+ReactDOM.createRoot(container).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
